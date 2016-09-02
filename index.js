7e0e7ffde507e14fa3018af9773a14939204a38a
Thiago Classen <tgclassen.dev@gmail.com>
2016-09-02 15:54:28 -0300

    auth test

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(function (req, res, next) {
     next();
 });
 
+//https://graph.facebook.com/me?fields=id&access_token=EAANFvPVGEqEBAESvGwkv9x1hIEt36MpRKIvMTluaKXi0tJpspsCACioFv7xvQR6ZChCHixfj1dW0oTqzhTVqWtmRZAmwzxXXqj8YiA4pMvPHkhMdWfavH3AmPpPpfugyBwzuz8kyF8S3APoxHoVYrKxYiXMWNkq0ZALgBw01gZDZD
+
 var props = require('./routes/props');
 app.use('/props', props);
 
--- routes/props.js
+++ routes/props.js
@@ -4,16 +4,31 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var parseJson = bodyParser.json();
 var parseUrlEncoded = bodyParser.urlencoded({ extended: false });
+var request = require('request');
+
+/*
+ *  checks of the access_token is valid, then pass the request forward.
+ */
+
+var auth = function (req, res, next) {
+    request('https://graph.facebook.com/me?fields=id&access_token=' + req.query.url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body).id)
+            next();
+        } else {
+            res.send('error', 401);
+        }
+    });
+}
 
 router.route('/')
-    .get(function (request, response) {
-        //response.sendFile(__dirname + '/mocks/props.JSON');
+    .get(auth, function (request, response) {
         response.sendFile(path.join(__dirname, '../mocks', 'props.JSON'));
     })
     .post(parseUrlEncoded, function (request, response) {
         console.log(request.body);
-        
-        response.status(200).json('OK: '+JSON.stringify(request.body));
+
+        response.status(200).json('OK: ' + JSON.stringify(request.body));
     });
 
 module.exports = router;
